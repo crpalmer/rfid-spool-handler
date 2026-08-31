d7b5d370e88b98904f7e07421864341a3f96aa07
Christopher R. Palmer <crpalmer@gmail.com>
2026-08-31 10:51:29 -0400

    wifi: refactor out the wifi code and make it loop on failures

--- rfid-spool-handler.py
+++ rfid-spool-handler.py
@@ -1,53 +1,14 @@
 from microdot import Microdot
 from microdot import Response
 
-import network
-import time
-
-Response.default_content_type = 'text/html'
-
 import json
 import machine
 
-def wifi_setup():
-    wlan = network.WLAN(network.WLAN.IF_AP)
-    wlan.config(ssid="rfid-spool-handler", password="password")
-    wlan.active(True)
-    portal = Microdot()
-    @portal.route('/')
-    async def index(request):
-        if "ssid" in request.args and "password" in request.args:
-            config = { "ssid": request.args["ssid"], "password": request.args["password"] }
-            with open("wifi.json", "w") as f:
-                json.dump(config, f)
-            machine.reset()
-            return "<h1>Wifi Config Saved</h1>"
-        return '<html><head><title>RFID Spool Handler (SETUP)</title></head><body><h1>RFID Spool Handler: Setup WiFi</h1><form><div>SSID: <input name="ssid" type="text" size="40"/></div><div>Password: <input name="password" type="password"/></div><input type="submit"/></form></body></html>'
-    portal.run(port=80)
+from wifi import wifi_connect
 
-try:
-    with open("wifi.json", "r") as f:
-        config = json.load(f)
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        print("Trying to connect to SSID: " + config["ssid"])
-        wlan.connect(config["ssid"], config["password"])
-        timeout = 30
-        while not wlan.isconnected():
-            time.sleep(1)
-            timeout -= 1
-            if timeout <= 0:
-                print("Couldn't connect to wifi, entering setup")
-                wifi_setup()
-except ValueError:
-    print("Malformed JSON in wifi.json, entering setup")
-    wifi_setup()
-except OSError:
-    print("No wifi.json, entering setup")
-    wifi_setup()
+wifi_connect()
 
-print('Connected on IP:', wlan.ifconfig()[0])
-
+Response.default_content_type = 'text/html'
 app = Microdot()
 
 @app.route('/')
